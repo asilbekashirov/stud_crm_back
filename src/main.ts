b7320f316eddea538231095ac886565b36b54502
asilbekashirov <asilbekashirov03@gmail.com>
2023-04-18 05:51:53 +0300

    refactor2

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  console.log('server is up ')
+  console.log('server is up and running');
   await app.listen(5000);
 }
 bootstrap();
